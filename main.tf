98c810c8fa64102c4181e6595937b2ee78c6d190
Calvin Seok <calvinseok@gmail.com>
2024-07-09 15:34:16 -0700

    terraform innit

---
+++ main.tf
@@ -0,0 +1,7 @@
+locals {
+    vpc_id = "vpc-0529197c524a60950"
+    subnet_id = "subnet-0b18f6be43597180c"
+    ssh_user = "ubuntu"
+    key_name = "devops"
+    private_key_path = "~/Downloads/devops.pem"
+}
